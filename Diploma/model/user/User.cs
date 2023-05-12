c18a1ebd501665818ea4ab7318a12b1dc5b37b90
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2023-05-13 01:27:54 +0400

    user add column hourse

--- Diploma/model/user/User.cs
+++ Diploma/model/user/User.cs
@@ -10,6 +10,7 @@ public class User
     [Required] public string LastName { get; set; } = string.Empty;
     [Required] public string Password { get; set; } = string.Empty;
     [Required] public int Balance { get; set; } = 0;
+    [Required] public int Hourse { get; set; } = 0;
     [Required] public Specialization Specialization { get; set; }
     
     public virtual string Role => "BaseUser";
