34631a721c787d06fbfd7bf9fa93ea504fd2511c
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2023-05-13 23:57:21 +0400

    update connection name

--- Diploma/Program.cs
+++ Diploma/Program.cs
@@ -34,9 +34,9 @@ void RegisterServices(IServiceCollection services)
     {
         Server = "cfif31.ru",
         Port = 3306,
-        UserID = "ISPr24-39_BeluakovDS",
-        Password = "ISPr24-39_BeluakovDS",
-        Database = "ISPr24-39_BeluakovDS_Kwork_Diplom_1",
+        UserID = "ISPr22-32_ChekriginKA",
+        Password = "ISPr22-32_ChekriginKA",
+        Database = "ISPr22-32_ChekriginKA_Kwork_Diplom_1",
         CharacterSet = "utf8"
     };
 
